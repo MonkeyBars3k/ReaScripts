99f879ba01a261572b25aa7eb195485e37c9a460
MonkeyBars3k <91908907+MonkeyBars3k@users.noreply.github.com>
2021-10-07 17:07:07 -0500

    Update Item Container Utils.lua

--- Item Containers/Item Container Utils.lua
+++ Item Containers/Item Container Utils.lua
@@ -1,7 +1,6 @@
 -- @description Tools for Item Container functionality – adapted from matthewjumpsoffbuildings's Glue Groups scripts
 -- @author MonkeyBars
--- @version 1.03
--- @noindex
+-- @version 1.04
 -- @provides [nomain] Item Container Utils.lua
 -- @link Forum https://forum.cockos.com/showthread.php?t=136273
 -- @about 
