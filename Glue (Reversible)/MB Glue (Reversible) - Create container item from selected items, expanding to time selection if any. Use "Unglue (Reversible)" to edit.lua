377f8c92fbeb803ccda9b6a5324ff61a979449bf
MonkeyBars3k <91908907+MonkeyBars3k@users.noreply.github.com>
2021-10-22 09:28:03 -0500

    Issue 43

---
+++ Glue (Reversible)/MB Glue (Reversible) - Create container item from selected items, expanding to time selection if any. Use "Unglue (Reversible)" to edit.lua
@@ -0,0 +1,13 @@
+-- @description MB Glue (Reversible): Create container item from selected items, expanding to time selection, if any
+-- @author MonkeyBars
+-- @version 1.25
+-- @changelog Add "Glue (Reversible) ignoring time selection" script (https://github.com/MonkeyBars3k/ReaScripts/issues/43)
+-- @provides [main] .
+-- @link Forum https://forum.cockos.com/showthread.php?t=136273
+-- @about Fork of matthewjumpsoffbuildings's Glue Groups scripts
+
+
+package.path = package.path .. ";" .. string.match(({reaper.get_action_context()})[2], "(.-)([^\\/]-%.?([^%.\\/]*))$") .. "?.lua"
+require("MB Glue (Reversible) Utils")
+
+glueGroup(true)
