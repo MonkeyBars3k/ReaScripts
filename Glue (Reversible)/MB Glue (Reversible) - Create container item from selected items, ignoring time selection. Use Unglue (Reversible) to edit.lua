554d1e0c821a718484a94844c8aed6321d770594
MonkeyBars3k <91908907+MonkeyBars3k@users.noreply.github.com>
2021-10-25 15:51:04 -0500

    iterate version

--- Glue (Reversible)/MB Glue (Reversible) - Create container item from selected items, ignoring time selection. Use Unglue (Reversible) to edit.lua
+++ Glue (Reversible)/MB Glue (Reversible) - Create container item from selected items, ignoring time selection. Use Unglue (Reversible) to edit.lua
@@ -1,7 +1,7 @@
 -- @description MB Glue (Reversible): Create container item from selected items, ignoring time selection
 -- @author MonkeyBars
--- @version 1.25
--- @changelog Add "Glue (Reversible) ignoring time selection" script (https://github.com/MonkeyBars3k/ReaScripts/issues/43)
+-- @version 1.27
+-- @changelog iterate version
 -- @provides [main] .
 -- @link Forum https://forum.cockos.com/showthread.php?t=136273
 -- @about Fork of matthewjumpsoffbuildings's Glue Groups scripts
